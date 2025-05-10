dfac8fce2c8afefb5403850aafb309abdeffb12c
ShahAlom47 <sahalom4729@gmail.com>
2025-05-10 23:43:10 +0600

    update:book mark  drawer

--- src/app/components/BookmarkContainer.tsx
+++ src/app/components/BookmarkContainer.tsx
@@ -12,25 +12,29 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import { BsFillJournalBookmarkFill } from "react-icons/bs";
+import { BiSolidRightArrow } from "react-icons/bi";
 
 const BookmarkContainer = () => {
   return (
     <div>
-      <Drawer >
-        <DrawerTrigger><BsFillJournalBookmarkFill /></DrawerTrigger>
-        <DrawerContent>
+      <Drawer>
+        <DrawerTrigger>
+          <BsFillJournalBookmarkFill />
+        </DrawerTrigger>
+        <DrawerContent className="  border-l-8  border-primary-border  ">
           <DrawerHeader className=" h-full">
-            <DrawerTitle className="bg-color-primary ">BookMark</DrawerTitle>
-            <DrawerDescription>
-                This action cannot be undone.
-                </DrawerDescription>
+            <DrawerTitle className="bg-color-primary py-3  border-b-4 border-primary-border px-2 text-gray-300">
+              BookMark
+            </DrawerTitle>
+            <DrawerDescription className=" p-4 relative h-full  ">
+              <DrawerClose className="absolute top-5/12 left-0 bg-primary-border  py-6 px-0.5 rounded-r-md">
+              <BiSolidRightArrow size={16} className=" text-gray-300" />
+                {/* <Button variant="outline">Cancel</Button> */}
+              </DrawerClose>
+              This action cannot be undone.
+            </DrawerDescription>
           </DrawerHeader>
-          <DrawerFooter>
-           
-            <DrawerClose>
-              <Button variant="outline">Cancel</Button>
-            </DrawerClose>
-          </DrawerFooter>
+          <DrawerFooter></DrawerFooter>
         </DrawerContent>
       </Drawer>
     </div>
